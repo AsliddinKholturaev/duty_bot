a7f8df399ecfd8bb11feec3debb54116b2b04cf0
Asliddin Kholturaev <a.kholturaev.dev@gmail.com>
2026-06-01 13:51:44 +0500

    migrations implemented

--- src/database/client/prismaClient.js
+++ src/database/client/prismaClient.js
@@ -1,15 +1,37 @@
 const { PrismaClient } = require("@prisma/client");
+const { Pool } = require("pg");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
 let prisma;
+let pool;
 
 function getPrismaClient() {
   if (!prisma) {
-    prisma = new PrismaClient();
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+    });
+
+    prisma = new PrismaClient({
+      adapter: new PrismaPg(pool),
+    });
   }
 
   return prisma;
 }
 
+async function closePrismaClient() {
+  if (prisma) {
+    await prisma.$disconnect();
+    prisma = null;
+  }
+
+  if (pool) {
+    await pool.end();
+    pool = null;
+  }
+}
+
 module.exports = {
   getPrismaClient,
+  closePrismaClient,
 };
